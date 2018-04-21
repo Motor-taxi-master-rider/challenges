a848aaf318dd2719c09977c3c5e9b6b333a9bf42
摩的大飚客 <gflame42@gmail.com>
2018-04-21 16:16:45 +0800

    :new: challenge 1 solved

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,18 +1,26 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 def load_words():
     """Load dictionary into a list and return list"""
-    pass
+    with open(DICTIONARY) as fh:
+        return [line[:-1] for line in fh.readlines()]
 
-def calc_word_value():
+
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    return sum(LETTER_SCORES.get(letter.upper(), 0) for letter in word)
 
-def max_word_value():
+
+def max_word_value(word_list=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    if word_list is None:
+        word_list = load_words()
+    return max(word_list, key=calc_word_value)
+
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    print(LETTER_SCORES)
+    print()
