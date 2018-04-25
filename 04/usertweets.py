2cae633eee3691706d01e7025df5378ca9e41975
摩的大飚客 <hzy1324@qq.com>
2018-04-25 13:27:45 +0800

    :wrench: In chanllenge 4: 1. fix unicode issue 2. concurrency improvement

--- 04/usertweets.py
+++ 04/usertweets.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import csv
 import os
 
@@ -16,16 +17,16 @@ Tweet = namedtuple('Tweet', 'id_str created_at text')
 
 
 class UserTweets(object):
-
     def __init__(self, handle, max_id=None):
         """Get handle and optional max_id.
         Use tweepy.OAuthHandler, set_access_token and tweepy.API
         to create api interface.
         Use _get_tweets() helper to get a list of tweets.
         Save the tweets as data/<handle>.csv"""
-        time=datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        self.output_file=os.path.join(DEST_DIR,f'{handle}_{time}.{EXT}')
-        self._handle=handle
+        time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        self.output_file = os.path.join(DEST_DIR, f'{handle}_{time}.{EXT}')
+        self._handle = handle
+        self._max_id = max_id
         self._tweets = list(self._get_tweets())
         self._save_tweets()
 
@@ -34,11 +35,11 @@ class UserTweets(object):
         See tweepy API reference: http://docs.tweepy.org/en/v3.5.0/api.html
         Use a list comprehension / generator to filter out fields
         id_str created_at text (optionally use namedtuple)"""
-        auth=tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
-        api=tweepy.API(auth)
-        for tweet in api.user_timeline(self._handle,count=NUM_TWEETS):
-            yield Tweet(id_str=tweet.id_str,created_at=tweet.created_at,text=tweet.text)
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        api = tweepy.API(auth)
+        for tweet in api.user_timeline(self._handle, count=NUM_TWEETS, max_id=self._max_id):
+            yield Tweet(id_str=tweet.id_str, created_at=tweet.created_at, text=tweet.text)
 
     def _save_tweets(self):
         """Use the csv module (csv.writer) to write out the tweets.
@@ -46,12 +47,11 @@ class UserTweets(object):
         Otherwise define them as: id_str created_at text
         You can use writerow for the header, writerows for the rows"""
 
-        with open(self.output_file,'w',encoding='utf-8') as fh:
-            writer=csv.writer(fh)
+        with open(self.output_file, 'w', encoding='utf-8', newline='') as fh:
+            writer = csv.writer(fh)
             writer.writerow(Tweet._fields)
             for tweet in self._tweets:
                 writer.writerow(tweet)
-
 
     def __len__(self):
         """See http://pybit.es/python-data-model.html"""
@@ -63,10 +63,10 @@ class UserTweets(object):
 
 
 if __name__ == "__main__":
-
-    for handle in ('pybites', 'izzy_spideypool', 'bbelderbos'):
-        print('--- {} ---'.format(handle))
-        user = UserTweets(handle)
-        for tw in user[:5]:
-            print(tw)
-        print()
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        wait_for = [executor.submit(UserTweets, handle) for handle in ('pybites', 'izzy_spideypool', 'bbelderbos')]
+        for future in as_completed(wait_for):
+            user = future.result()
+            for tw in user[:5]:
+                print(tw)
+            print()
