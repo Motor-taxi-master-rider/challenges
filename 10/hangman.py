6ef75d82f1e7ec67a8ddee8207531cfb639fda6b
摩的大飚客 <gflame42@gmail.com>
2018-05-06 14:28:56 +0800

    :new: challenge 10：fix logic and add terminal color

--- 10/hangman.py
+++ 10/hangman.py
@@ -5,6 +5,7 @@ from string import ascii_lowercase
 
 from graphics import hang_graphics
 from movies import get_movie as get_word  # keep interface generic
+from termcolor import colored
 
 ASCII = list(ascii_lowercase)
 PLACEHOLDER = '_'
@@ -13,7 +14,6 @@ PLACEHOLDER = '_'
 class Hangman(object):
     def __init__(self, word: str):
         word = word.lower()
-        self._guessed_character = set()
         self._word: defaultdict = self._construct_word(word)
         self._guess: list = [PLACEHOLDER if character in ASCII else character
                              for character in word]
@@ -30,29 +30,31 @@ class Hangman(object):
     @types.coroutine
     def _hangman_popper(self):
         """print a hangman graph if guess is not right"""
+        guessed_character = set()
         graphics = hang_graphics()
         graph = next(graphics)
+
         while True:
             guess = yield False
-            if guess not in self._guessed_character:
-                self._guessed_character.add(guess)
+            if guess not in guessed_character:
+                guessed_character.add(guess)
                 if guess in self._word:
+                    print(f'{colored(len(self._word[guess]),"green")} of {colored(guess,"green")} in the word.')
                     for index in self._word.pop(guess):
                         self._guess[index] = guess
                     if not self._word:
                         yield True
                 else:
-                    print(f'{guess} is not in the word!\n'
+                    print(f'{colored(guess,"green")} is not in the word!\n'
                           f'{graph}\n')
                     try:
                         graph = next(graphics)
                     except StopIteration:
                         raise NoChance
-
             else:
-                print(f'You have guessed {guess}, please choose another character.')
+                print(f'You have guessed {colored(guess,"green")} before, please choose another character.\n')
 
-    def current_puzzle(self, result=False):
+    def current_puzzle(self, result: bool = False) -> str:
         """if result True, fill up the answer"""
         if result:
             for character, indexs in self._word.items():
@@ -61,7 +63,7 @@ class Hangman(object):
         return ' '.join(self._guess)
 
     def guess(self, character: str):
-        print(f'You guessed {character}.')
+        print(f'You guessed {colored(character,"green")}.')
         result = self._hangman.send(character.lower())
         return result
 
